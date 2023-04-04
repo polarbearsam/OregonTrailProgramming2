7f054d66e552f5f9170d749f0a9aa41e14a34e78
SamTheGamer39 <72585301+samthegamer39@users.noreply.github.com>
2023-04-03 23:05:36 -0400

    Updated GUI code.

--- app/src/main/java/com/example/oregontrail/MainActivity.java
+++ app/src/main/java/com/example/oregontrail/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.oregontrail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,51 +20,36 @@ public class MainActivity extends AppCompatActivity {
 
         // GUI Elements
         final Button start = findViewById(R.id.startButton);
-        // TODO: Add all GUI Elements here.
-        //Start button code
-        final Button start = findViewById(R.id.startButton);
+        final TextView display = (TextView) findViewById(R.id.OreganWelcome);
+        final TextView end = (TextView) findViewById(R.id.endButton);
+        final TextView menu = (TextView) findViewById(R.id.optionsButton);
+        final TextView menuDisplay = (TextView) findViewById(R.id.menuDisplay);
 
+        // FIXME: Several issues with the GUI, not entirely sure why.
         start.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n") // Unclear why this is needed, but it is.
             @Override
             public void onClick(View view) {
-                int day = 0;
-                final TextView display = (TextView) findViewById(R.id.OreganWelcome);
-                final TextView end = (TextView) findViewById(R.id.endButton);
-                final TextView menu = (TextView) findViewById(R.id.optionsButton);
-                final TextView menuDisplay = (TextView) findViewById(R.id.menuDisplay);
                 //Code to hide start button goes here
-                boolean endDay = false;
+                int day = 1;
                 int temp = 100;
-                day = 1;
 
-                if (end.isEnabled()) { //If the end day button is clicked
-                    endDay = true;
+                // FIXME: This loop throws exceptions?
+                while (true) {
+                    boolean endDay = false;
+
+                    if (end.isEnabled()) { //If the end day button is clicked
+                        day++;
+                        display.setText("Day " + day);
+                    }
+
+                    if (menu.isEnabled()) { //When the menu button is clicked
+                        //Displays condition
+                        menuDisplay.setText("Health: " + temp + "\nThirst: " + temp + "\nHunger: " + temp + "\nMood: " + temp);
+
+                    }
                 }
-                if (endDay = true) {
-                    day = day + 1;
-                }
-
-                if (day == 1) {
-                    display.setText("Day 1");
-                } else if (day == 2) {
-                    display.setText("Day 2");
-                } else if (day == 3) {
-                    display.setText("Day 3");
-                } else if (day == 4) {
-                    display.setText("Day 4")
-                } else if (day == 5) {
-                    display.setText("Day 5");
-                } // And so on for however many days!
-
-                if (menu.isEnabled()){ //When the menu button is clicked
-                    //Displays condition
-                    menuDisplay.setText("Health: " + temp + "\nThirst: " + temp + "\nHunger: " + temp + "\nMood: " + temp);
-
-                }
-
             }
         });
-
-        // TODO: Implement functionality.
     }
 }
