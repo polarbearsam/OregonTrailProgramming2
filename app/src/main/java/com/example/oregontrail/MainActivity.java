67a6e1cf9458c851a3861a261380812abf768175
DestinyMorrison <112641813+DestinyMorrison@users.noreply.github.com>
2023-04-04 13:33:12 -0400

    lll

--- app/src/main/java/com/example/oregontrail/MainActivity.java
+++ app/src/main/java/com/example/oregontrail/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -26,15 +25,15 @@ public class MainActivity extends AppCompatActivity {
         final Button map = findViewById(R.id.mapButton);
         final Button menu = findViewById(R.id.optionsButton);
         final Button start = findViewById(R.id.startButton);
-        final TextView display = findViewById(R.id.menuDisplay);
-        final TextView location = findViewById(R.id.locationText);
         final TextView title = findViewById(R.id.oregonWelcome);
+        final TextView display = findViewById(R.id.menuDisplay);
 
         ArrayList<Place> Towns = new ArrayList<>();
         Towns.add(new Place("Independence, Missouri", 1));
         Towns.add(new Place("Beaverton", 50));
 
         Person hattie = new Person("Hattie Campbell");
+        hattie.nextDay();
 
         // FIXME: Several issues with the GUI, not entirely sure why.
         start.setOnClickListener(view -> {
@@ -43,16 +42,18 @@ public class MainActivity extends AppCompatActivity {
             int temp = 100;
 
             end.setOnClickListener(view1 -> {
+                boolean inCity = false;
                 day[0]++;
-                hattie.nextDay();
-                location.setText("On the trail.");
                 for (int i = 0; i < Towns.size(); i++) {
                     if(Towns.get(i).getLocation() == day[0]) {
-                        location.setText(Towns.get(i).getName());
+                        title.setText(Towns.get(i).getName());
+                        inCity = true;
                         break;
                     }
                 }
-                title.setText("Day " + day[0]);
+                if(!inCity) {
+                    title.setText("Day " + day[0]);
+                }
             });
 
             map.setOnClickListener(view1 -> {
@@ -66,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
             menu.setOnClickListener(view12 -> {
                 //Displays condition
-                display.setText("Health: " + hattie.getHealth() + "\nThirst: " + hattie.getThirst() + "\nHunger: " + hattie.getHunger() + "\nMood: " + hattie.getEmotion());
+                display.setText("Hattie Campbell-- Stats" + "\nHealth: " + temp + "\nThirst: " + temp + "\nHunger: " + temp + "\nMood: " + temp);
             });
         });
     }
