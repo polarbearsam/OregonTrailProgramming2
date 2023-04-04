3ec072890dda72bc1bdeb28586b0d042ab9d7e35
DestinyMorrison <112641813+destinymorrison@users.noreply.github.com>
2023-04-03 21:48:34 -0400

    Corrected a bad merge.

--- app/src/main/java/com/example/oregontrail/MainActivity.java
+++ app/src/main/java/com/example/oregontrail/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.oregontrail;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,10 +17,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // GUI Elements
+        //Start button code
         final Button start = findViewById(R.id.startButton);
-        // TODO: Add all GUI Elements here.
 
-        // TODO: Implement functionality.
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int day = 0;
+                final TextView display = (TextView) findViewById(R.id.OreganWelcome);
+                final TextView end = (TextView) findViewById(R.id.endButton);
+                final TextView menu = (TextView) findViewById(R.id.optionsButton);
+                final TextView menuDisplay = (TextView) findViewById(R.id.menuDisplay);
+                //Code to hide start button goes here
+                boolean endDay = false;
+                int temp = 100;
+                day = 1;
+
+                if (end.isEnabled()) { //If the end day button is clicked
+                    endDay = true;
+                }
+                if (endDay = true) {
+                    day = day + 1;
+                }
+
+                if (day == 1) {
+                    display.setText("Day 1");
+                } else if (day == 2) {
+                    display.setText("Day 2");
+                } else if (day == 3) {
+                    display.setText("Day 3");
+                } else if (day == 4) {
+                    display.setText("Day 4");
+                } else if (day == 5) {
+                    display.setText("Day 5");
+                } // And so on for however many days!
+
+                if (menu.isEnabled()){ //When the menu button is clicked
+                    //Displays condition
+                    menuDisplay.setText("Health: " + temp + "\nThirst: " + temp + "\nHunger: " + temp + "\nMood: " + temp);
+
+                }
+
+            }
+        });
+
     }
 }
