31a1339700989f1d1abc41836cfec787fe801fb8
SamTheGamer39 <72585301+samthegamer39@users.noreply.github.com>
2023-04-03 23:29:03 -0400

    Finally made GUI functional.

--- app/src/main/java/com/example/oregontrail/MainActivity.java
+++ app/src/main/java/com/example/oregontrail/MainActivity.java
@@ -13,43 +13,34 @@ import android.widget.TextView;
  */
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n") // Unclear why this is needed, but it is.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // GUI Elements
+        final Button end = findViewById(R.id.endButton);
+        final Button menu = findViewById(R.id.optionsButton);
         final Button start = findViewById(R.id.startButton);
-        final TextView display = (TextView) findViewById(R.id.OreganWelcome);
-        final TextView end = (TextView) findViewById(R.id.endButton);
-        final TextView menu = (TextView) findViewById(R.id.optionsButton);
-        final TextView menuDisplay = (TextView) findViewById(R.id.menuDisplay);
+        final TextView display = findViewById(R.id.oregonWelcome);
+        final TextView menuDisplay = findViewById(R.id.menuDisplay);
 
         // FIXME: Several issues with the GUI, not entirely sure why.
-        start.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n") // Unclear why this is needed, but it is.
-            @Override
-            public void onClick(View view) {
-                //Code to hide start button goes here
-                int day = 1;
-                int temp = 100;
+        start.setOnClickListener(view -> {
+            //Code to hide start button goes here
+            final int[] day = {1};
+            int temp = 100;
 
-                // FIXME: This loop throws exceptions?
-                while (true) {
-                    boolean endDay = false;
+            end.setOnClickListener(view1 -> {
+                day[0]++;
+                display.setText("Day " + day[0]);
+            });
 
-                    if (end.isEnabled()) { //If the end day button is clicked
-                        day++;
-                        display.setText("Day " + day);
-                    }
-
-                    if (menu.isEnabled()) { //When the menu button is clicked
-                        //Displays condition
-                        menuDisplay.setText("Health: " + temp + "\nThirst: " + temp + "\nHunger: " + temp + "\nMood: " + temp);
-
-                    }
-                }
-            }
+            menu.setOnClickListener(view12 -> {
+                //Displays condition
+                menuDisplay.setText("Health: " + temp + "\nThirst: " + temp + "\nHunger: " + temp + "\nMood: " + temp);
+            });
         });
     }
 }
