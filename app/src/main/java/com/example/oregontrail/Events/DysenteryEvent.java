b58d780341550e9e4319354d72776b63b4c78263
SamTheGamer39 <72585301+SamTheGamer39@users.noreply.github.com>
2023-05-13 21:10:38 -0400

    Added more random events.

--- app/src/main/java/com/example/oregontrail/Events/DysenteryEvent.java
+++ app/src/main/java/com/example/oregontrail/Events/DysenteryEvent.java
@@ -1,40 +1,40 @@
 /**
- * File Name: CholeraEvent.java
+ * File Name: DysenteryEvent.java
  * Date: May 13, 2023
  * @author Samuel Freer
- * Description: Gives a person cholera.
+ * Description: Gives a person dysentery.
  */
 package com.example.oregontrail.Events;
 
-import com.example.oregontrail.Conditions.Cholera;
+import com.example.oregontrail.Conditions.Dysentery;
 import com.example.oregontrail.Event;
 import com.example.oregontrail.MainActivity;
 import com.example.oregontrail.Person;
 import com.example.oregontrail.Wagon;
 
 /**
- * Creates an event which gives a random person cholera.
+ * Creates an event which gives a random person dysentery.
  */
-public class CholeraEvent extends Event {
+public class DysenteryEvent extends Event {
 
     /**
-     * Creates the ContractsCholera event.
+     * Creates the DysenteryEvent event.
      * @param frequency the frequency at which the event will occur. Must be between 0 and 1.
      */
-    public CholeraEvent(double frequency) {
-        super("Contracts Cholera", frequency);
+    public DysenteryEvent(double frequency) {
+        super("Contracts Dysentery", frequency);
     }
 
     /**
-     * Gives a random person cholera.
+     * Executes the event.
      * @param wagon the wagon that the event is happening to.
      * @return the text output for the event.
      */
     @Override
     public String onEvent(Wagon wagon) {
         Person person = wagon.getPeople().get(MainActivity.randomValue(wagon.getPeople().size(), 0));
-        person.addCondition(new Cholera(10));
+        person.addCondition(new Dysentery(10));
 
-        return person.getName() + " contracted Cholera.";
+        return person.getName() + " contracted Dysentery.";
     }
 }
